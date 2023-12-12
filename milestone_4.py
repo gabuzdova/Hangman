e8671ccf905882b4795970264e5e648b4ac2af8a
gabuzdova <zuzana.gabuzdova@gmail.com>
2023-12-12 16:27:40 +0000

    adds else statement in check guess function

--- milestone_4.py
+++ milestone_4.py
@@ -5,9 +5,10 @@ class Hangman:
     def __init__(self, word_list, num_lives=5):    
         self.word_list=["apple", "pineapple", "pear", "banana", "mangosteen"]
         self.word=random.choice(word_list)
-        self.word_guessed=[]
+        self.the_letter_guessed_by_the_user=[]
         self.list_of_guesses=[]
-        self.num_letters=[]
+        self.num_letters=0
+        self.word_guessed=len(self.word) * "_"
     
     def ask_for_input(self):
             while True:
@@ -20,24 +21,23 @@ class Hangman:
                     self.check_guess()
                     self.list_of_guesses.append(guess_the_letter)
 
-    def check_guess (self,word_guessed):
-            self.word_guessed.lower()
-            if self.word_guessed in self.word :
-                print(f"Good guess! {self.word_guessed} is in the word.")
+    def check_guess (self, the_letter_guessed_by_the_user):
+            self.the_letter_guessed_by_the_user.lower()
+            if self.the_letter_guessed_by_the_user in self.word :
+                print(f"Good guess! {self.the_letter_guessed_by_the_user} is in the word.")
+                for each_letter in self.word:
+                     if each_letter == the_letter_guessed_by_the_user:
+                        self.word_guessed +=each_letter
+                self.num_letters-=1          
             else:
-                print(f"Sorry, {self.word_guessed} is not in the word. Try again." )
+                print(f"Sorry, {self.the_letter_guessed_by_the_user} is not in the word. Try again." )
 
-    ask_for_input(None)
+print(Hangman.word_guessed)
 
+#In the if block of your check_guess method, after your print statement, do the following:
 
- 
-    #Create an elif statement that checks if the guess is already in the list_of_guesses
-  #  In the body of the elif statement, print a message saying "You already tried that letter!"
-
-#If the guess is a single alphabetical character and it's not already in the list_of_guesses, create an else block and call the check_guess method. Remember to pass the guess as an argument to this method.
-
-#Finally, append the guess to the list_of_guesses.
-#Ensure you do this in the else block of this function, rather than inside the check_guess method, so that the letter can be appended to the list_of_guesses in both conditions.
-
-#Step 3:
-#Call the ask_for_input method to test your code. 
+ #   Create a for-loop that will loop through each letter in the word
+  #  Within the for-loop, do the following:
+   #     Create an if statement that checks if the letter is equal to the guess
+    #    In the if block, replace the corresponding "_" in the word_guessed with the guess. HINT: You can index the word_guessed at the position of the letter and assign it to the letter
+    #Outside the for-loop, reduce the variable num_letters by 1
